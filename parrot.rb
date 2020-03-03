495142cc1138c3546cc7c7c8681573c87e81e7fd
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-03 14:57:33 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,4 +1,4 @@
 def parrot (phrase = "Squawk!""Pretty bird!")
-  puts phrase ="Squawk!Pretty bird!"
-  return phrase = "Squawk!""Pretty bird!"
+  puts phrase ="Pretty bird!"
+  return phrase = "Squawk!"
 end
